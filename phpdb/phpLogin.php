f2a72bddbc0310ffbece615f13206a66074a7726
Andrew <twintrool@gmail.com>
2016-05-17 23:44:39 -0600

    fixed i think

--- phpdb/phpLogin.php
+++ phpdb/phpLogin.php
@@ -3,9 +3,6 @@
 $username = htmlspecialchars($_POST['username']);
 $p = htmlspecialchars($_POST['pass']);
 
-echo $username;
-echo $p;
-
 $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
@@ -18,15 +15,13 @@ $stmt = $db->prepare("SELECT id FROM users WHERE username=:username AND password
 $stmt->execute(array(':username' => $username, ':password' => $p));
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-var_dump($rows);
-
 if(sizeof($rows) == 1){
-  echo "you made it";
-
+    echo "you made it";
+//    header('Location: /phpsurvey/phpresults.php');
 }
 else
 {
-
+    
 }
 
 ?>
