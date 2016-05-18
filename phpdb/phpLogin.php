d904b44299efcdd2f541d3a4ab36c54e2e92dc8a
Andrew <twintrool@gmail.com>
2016-05-17 22:53:09 -0600

    spelling

--- phpdb/phpLogin.php
+++ phpdb/phpLogin.php
@@ -1,14 +1,15 @@
 <?php
 
-$username = htmlspecialchars($_POST['usernam']);
+$username = htmlspecialchars($_POST['username']);
 $p = htmlspecialchars($_POST['pass']);
 
 $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
 $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
 $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
 $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD'); 
+$dbName = 'death_watch';
 
-$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword););
+$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 $result = $db->query('SELECT id FROM users WHERE username=:userName AND password=:password');
 $stmt->execute(array(':username' => $username, ':passwrod' => $password));
