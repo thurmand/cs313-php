a95a0c0cc08beb9c2f89203d7d90d5086724ce08
Andrew <twintrool@gmail.com>
2016-05-17 23:38:54 -0600

    fixed php file

--- phpdb/phpLogin.php
+++ phpdb/phpLogin.php
@@ -3,26 +3,30 @@
 $username = htmlspecialchars($_POST['username']);
 $p = htmlspecialchars($_POST['pass']);
 
-$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
-$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
-$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD'); 
+echo $username;
+echo $p;
+
+$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 $dbName = 'death_watch';
 
 $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-$stmt = $db->query('SELECT id FROM users WHERE username=:userName AND password=:password');
+$stmt = $db->prepare("SELECT id FROM users WHERE username=:username AND password=:password");
+$stmt->execute(array(':username' => $username, ':password' => $p));
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-if($stmt == false){
-    echo 'false';
+var_dump($rows);
+
+if(sizeof($rows) == 1){
+  echo "you made it";
+
 }
 else
 {
-    echo 'true';
+
 }
-/*$stmt->execute(array(':username' => $username, ':passwrod' => $password));
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);*/
 
-
-
-?> 
+?>
