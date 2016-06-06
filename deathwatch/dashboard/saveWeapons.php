3a98ab792190febe0e14c19d083bcf0fd43ce4f6
Andrew <twintrool@gmail.com>
2016-06-06 17:48:16 -0600

    saveWeapons

---
+++ deathwatch/dashboard/saveWeapons.php
@@ -0,0 +1,21 @@
+<?PHP
+
+require("../dbConnect.php");
+session_start();
+
+$userID = $_SESSION['userID'];
+
+$selected = json_decode($_POST['selected']);
+$i = 0;
+
+var_dump($selected);
+/*foreach($selected as $item)
+    $db->prepare("INSERT INTO users_weapons
+                        VALUES(:userID, :selected)")){
+
+    $stmt->execute(array(':userID' => $userID, ':selected' => $item[$i]));
+    $i++;
+}*/
+
+header('Location: index.php');
+?>
