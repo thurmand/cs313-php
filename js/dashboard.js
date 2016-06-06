9b8edebca00c3c28deb90950633f6d3e297d6c20
thurmand <twintrool@gmail.com>
2016-06-06 11:41:45 -0600

    edit weapon sentace

--- js/dashboard.js
+++ js/dashboard.js
@@ -182,7 +182,7 @@ function editWeapons(){
         xhttp.open("GET", "getWeapons.php", true);
         xhttp.send();
         
-        title[2].innerHTML += "<div style='font-sze:1.2vw'>Click on a Weapon to add/remove</div>"
+        title[2].innerHTML += "<div style='font-size:1.2vw'>Click on a Weapon to add/remove</div>"
         weaponList.style.display = "flex"
         weaponList.style.flexDirection = "row"
         weaponList.style.flexWarp = "wrap"
