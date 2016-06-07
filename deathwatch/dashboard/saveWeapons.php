cf9646ea108e7ccc92a81b48d5427816d9dd613c
Andrew <twintrool@gmail.com>
2016-06-06 18:05:31 -0600

    weapon click

--- deathwatch/dashboard/saveWeapons.php
+++ deathwatch/dashboard/saveWeapons.php
@@ -8,6 +8,7 @@ $userID = $_SESSION['userID'];
 $selected = json_decode($_POST['selected']);
 $i = 0;
 echo $i;
+echo $selected;
 var_dump($selected);
 /*foreach($selected as $item)
     $db->prepare("INSERT INTO users_weapons
