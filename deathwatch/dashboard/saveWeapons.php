e5d8d32a5c68686f3e16535e972cffae83319f67
Andrew <twintrool@gmail.com>
2016-06-06 20:46:36 -0600

    weapon click

--- deathwatch/dashboard/saveWeapons.php
+++ deathwatch/dashboard/saveWeapons.php
@@ -9,10 +9,11 @@ $item = json_decode($_GET['selected']);
 var_dump($item);
 
 foreach($item as $value){
-    echo $value;
-   // $db->prepare("INSERT INTO users_weapons
-   //                 VALUES(:userID, :selected)");
-   // $stmt->execute(array(':userID' => $userID, ':selected' => $value));
+   
+   $db->prepare("INSERT INTO users_weapons (user_id, weapon_id)
+                SELECT :userID, :selected FROM dual
+                WHERE NOT EXISTS (SELECT * FROM users_weapons WHERE weapon_id = :selected AND user_id = :userID)");
+   $stmt->execute(array(':userID' => $userID, ':selected' => $value));
 }
 //header('Location: index.php');
 ?>
